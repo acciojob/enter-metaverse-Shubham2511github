e74f5dc50261ea5a548caaa24f370ca2c854f5d4
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-06-15 23:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 //your JS code here. If required.
-let para = document.getElementById("status");
-let button =  document.getElementById("enterBtn");
-let h1 = document.getElementById("display");
+var para = document.getElementById("status");
+var button =  document.getElementById("enterBtn");
+var h1 = document.getElementById("display");
 button.addEventListener('click', ()=>{
 	setTimeout(()=>{
-	para.textContent =  "Entered Metaverse";
-	//para.parentNode.replaceChild(h1,para);
+	let h1 = document.createElement("h1");
+	h1.textContent = "Entered Metaverse";
+	para.parentNode.replaceChild(h1,para);
  },4000);
 	
 });
