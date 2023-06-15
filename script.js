2099623d00c7b7e122123e0f3de7062ace2bed35
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-06-15 23:30:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ let para = document.getElementById("status");
 let button =  document.getElementById("enterBtn");
 let h1 = document.getElementById("display");
 button.addEventListener('click', ()=>{
+	setTimeout(()=>{
 	let h1 = document.createElement("h1");
 	h1.textContent = "Entered Metaverse";
 	para.parentNode.replaceChild(h1,para);
+ },4000);
+	
 });
