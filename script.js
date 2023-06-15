6f09337bfb4e34158e35a1a7168818dcfad5a807
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-06-15 23:25:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let para = document.getElementById("status");
+let button =  document.getElementById("enterBtn");
+let h1 = document.getElementById("display");
+button.addEventListener('click', ()=>{
+	let h1 = document.createElement("h1");
+	h1.textContent = "Entered Metaverse";
+	para.parentNode.replaceChild(h1,para);
+});
